cfeade4d23290be8a1d5233f063700a6fb82971f
André <ruland@outlook.de>
2020-12-16 15:38:44 +0100

    Repository implementiert im EF und ein wenig Logik

---
+++ ppedv.Druckverwaltung/ppedv.Druckverwaltung.Data.EF/EfRepository.cs
@@ -0,0 +1,52 @@
+﻿using ppedv.Druckverwaltung.Model;
+using ppedv.Druckverwaltung.Model.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ppedv.Druckverwaltung.Data.EF
+{
+    public class EfRepository : IRepository
+    {
+        EfContext context = new EfContext();
+
+        public void Add<T>(T entity) where T : Entity
+        {
+            //if (typeof(T) == typeof(Patient))
+            //    context.Patienten.Add(entity as Patient);
+            context.Set<T>().Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : Entity
+        {
+            context.Set<T>().Remove(entity);
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : Entity
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public T GetById<T>(int id) where T : Entity
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public IQueryable<T> Query<T>() where T : Entity
+        {
+            return context.Set<T>();
+        }
+
+        public int Save()
+        {
+            return context.SaveChanges();
+        }
+
+        //nur web; bei desktop nur SaveChanges
+        public void Update<T>(T entity) where T : Entity
+        {
+            var loaded = GetById<T>(entity.Id);
+            if (loaded != null)
+                context.Entry(loaded).CurrentValues.SetValues(entity);
+        }
+    }
+}
--- ppedv.Druckverwaltung/ppedv.Druckverwaltung.Logic/Core.cs
+++ ppedv.Druckverwaltung/ppedv.Druckverwaltung.Logic/Core.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ppedv.Druckverwaltung.Model;
+using ppedv.Druckverwaltung.Model.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +10,22 @@ namespace ppedv.Druckverwaltung.Logic
 {
     public class Core
     {
-        public Core()
+        public IRepository Repository { get; private set; }
+
+        public Core(IRepository repo) //depenceny injection in here
         {
-           
+            Repository = repo;
         }
+
+        public Druckauftrag GetLongestDruckauftrag()
+        {
+            return Repository.Query<Druckauftrag>().Where(x => x.Status == Druckstatus.Fertig)
+                                                   .OrderBy(x => x.Ende - x.Start)
+                                                   .FirstOrDefault();
+        }
+
+
+        public Core() : this(new Data.EF.EfRepository())
+        { }
     }
 }

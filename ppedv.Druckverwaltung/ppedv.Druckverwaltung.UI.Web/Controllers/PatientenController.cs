ece2151b3399da5c3d53fbd55240528b8aba0577
André <ruland@outlook.de>
2020-12-16 16:19:45 +0100

    Easy Web UI

---
+++ ppedv.Druckverwaltung/ppedv.Druckverwaltung.UI.Web/Controllers/PatientenController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ppedv.Druckverwaltung.Logic;
+using ppedv.Druckverwaltung.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ppedv.Druckverwaltung.UI.Web.Controllers
+{
+    public class PatientenController : Controller
+    {
+        Core core = new Core();
+
+        // GET: PatientenController
+        public ActionResult Index()
+        {
+            return View(core.Repository.GetAll<Patient>());
+        }
+
+        // GET: PatientenController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(core.Repository.GetById<Patient>(id));
+        }
+
+        // GET: PatientenController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PatientenController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Patient p)
+        {
+            try
+            {
+                core.Repository.Add<Patient>(p);
+                core.Repository.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PatientenController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(core.Repository.GetById<Patient>(id));
+        }
+
+        // POST: PatientenController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Patient p)
+        {
+            try
+            {
+                core.Repository.Update<Patient>(p);
+                core.Repository.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PatientenController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(core.Repository.GetById<Patient>(id));
+        }
+
+        // POST: PatientenController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                var p = core.Repository.GetById<Patient>(id);
+                if(p!=null)
+                {
+                    core.Repository.Delete<Patient>(p);
+                    core.Repository.Save();
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ppedv.Druckverwaltung/ppedv.Druckverwaltung.UI.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ppedv.Druckverwaltung.UI.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
